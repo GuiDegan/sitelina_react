bcaf2b3ad2fe87ddf643100f112dbb1e9a0d39ea
Guilherme Degan <guilhermedegan@gmail.com>
2020-11-03 20:15:06 -0300

    Update de arquivos.

---
+++ src/components/botaosaiba.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+function BotaoSaiba() {
+    return <button className="botao_saibamais">SAIBA MAIS</button>
+}
+
+export default BotaoSaiba;
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ import Paragrafo from "../components/paragrafo"
 import Retangulo from "../components/retangulo"
 import LinhaVertical from "../components/linhavertical"
 import ImagemSecaoSomos from "../components/imagemsecao"
+import BotaoSaiba from "../components/botaosaiba"
 
 function Home() {
   return (
@@ -17,26 +18,80 @@ function Home() {
       <main className="conteudo d-flex">
         <SideBar></SideBar>
         <Container>
-          <Row>
-            <Col md="11" sm="12">
-              <Navbar />
-            </Col>
-            <Col md="12">
-              <Title />
-            </Col>
-            <Col md="12">
-              <TitlePeso />
-            </Col>
-            <Col md="4">
-              <Paragrafo />
-            </Col>
-            <Col md="7">
-              <ImagemSecaoSomos />
-            </Col>
-            <Col md="1">
-              <Retangulo />
-            </Col>
-          </Row>
+          <section>
+            <Row>
+              <Col md="11" sm="12">
+                <Navbar />
+              </Col>
+              <Col md="12">
+                <Title />
+              </Col>
+              <Col md="12">
+                <TitlePeso />
+              </Col>
+              <Col md="4">
+                <Paragrafo />
+              </Col>
+              <Col md="7">
+                <ImagemSecaoSomos />
+              </Col>
+              <Col md="1">
+                <Retangulo />
+              </Col>
+            </Row>
+            <Row>
+              <Col md="4">
+                <LinhaVertical />
+              </Col>
+            </Row>
+            <Row>
+              <Col md="6">
+                <BotaoSaiba />
+              </Col>
+            </Row>
+          </section>
+          <section className="secao2">
+            <Row>
+              <Col md="1">
+                <Retangulo />
+              </Col>
+              <Col md="7">
+                <ImagemSecaoSomos />
+              </Col>
+              <Col md="4">
+                <Paragrafo />
+              </Col>
+            </Row>
+          </section>
+          <section>
+            <Row>
+              <Col md="12">
+                <Title />
+              </Col>
+              <Col md="12">
+                <TitlePeso />
+              </Col>
+              <Col md="4">
+                <Paragrafo />
+              </Col>
+              <Col md="7">
+                <ImagemSecaoSomos />
+              </Col>
+              <Col md="1">
+                <Retangulo />
+              </Col>
+            </Row>
+            <Row>
+              <Col md="4">
+                <LinhaVertical />
+              </Col>
+            </Row>
+            <Row>
+              <Col md="6">
+                <BotaoSaiba />
+              </Col>
+            </Row>
+          </section>
         </Container>
       </main>
     </Fragment>
