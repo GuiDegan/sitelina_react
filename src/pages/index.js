543deb5e7c6a41e91df30fc9179a7c7d8ed72d00
Guilherme Degan <guilhermedegan@gmail.com>
2020-11-14 04:48:51 -0300

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -68,7 +68,7 @@ function Home() {
             </Row>
             <Row>
               <Col md="4">
-                <Paragrafo paragrafo="A Lina é a mais nova infraestrutura de pagamentos do mercado brasileiro. Somos um hub de conectividade para FinTechs e negócios digitais, e nascemos com o propósito de transformar o Sistema Financeiro Nacional, promovendo interoperabilidade e convivência entre os meios atuais e os novos ecossistemas de pagamentos instantâneos PIX e Open Banking" />
+                <Paragrafo paragrafo="A Lina é a mais nova infraestrutura de pagamentos do mercado brasileiro. Somos um hub de conectividade para FinTechs e negócios digitais, e nascemos com o propósito de transformar o Sistema Financeiro Nacional, promovendo interoperabilidade e convivência entre os meios atuais e os novos ecossistemas de pagamentos instantâneos PIX e Open Banking." />
               </Col>
               <Col md="7">
                 <ImagemSecaoSomos />
